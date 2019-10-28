8a3214ce717a2c6681861f49d7bc2992ce04ed75
Gladibeth <gladibethdelos@gmail>
2019-10-28 13:46:29 -0400

    Add navbar fixed

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -19,7 +19,7 @@ $('.slider-nav').slick({
 	focusOnSelect: true
 });
 
-$('a[data-slide]').click(function(e) {
+$('a[data-slide]').click(function (e) {
 	e.preventDefault();
 	var slideno = $(this).data('slide');
 	$('.slider-nav').slick('slickGoTo', slideno - 1);
@@ -29,4 +29,25 @@ $('.main-banne-content__logos').slick({
 	slidesToScroll: 1,
 	autoplay: true,
 	autoplaySpeed: 4000,
+});
+
+// Menú fixed
+$(window).scroll(function () {
+	if ($(document).scrollTop() > 70 && ($(window).width() >= 0)) {
+		$('.navbar-fixed-js').addClass('fixed-top');
+		$('.nav-link').addClass('fixed-color');
+		$('.btn-white').addClass('btn-blue');
+		// $('.nav-top__header').addClass('nav-top__header--detele');
+		// $('.hamburger-inner').addClass('js-hamburger');
+		// $("#iso").addClass('img-size').attr('src', 'assets/img/Logo_CM.png').removeClass('scroll-up');
+
+	} else {
+		$('.navbar-fixed-js').removeClass('fixed-top');
+		$('.nav-link').removeClass('fixed-color');
+		$('.btn-white').removeClass('btn-blue');
+		// $('.nav-top__header').removeClass('nav-top__header--detele');
+		// $('.hamburger-inner').removeClass('js-hamburger');
+		// $("#iso").removeClass('img-size').attr('src', 'assets/img/logo-color.png').removeClass('scroll-up');
+
+	}
 });
